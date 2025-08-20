89109627fc9b1b01866d088bd0cecccad6200e14
freemanzwin <freemanzwin@gmail.com>
2025-08-20 12:04:13 +0900

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -5,7 +5,7 @@ import { cors } from 'hono/cors';
 const app = new Hono();
 
 app.use("/*", cors());
-app.get("/", (c) => c.text("Hello world!"));
+app.get("/", (c) => c.text("Hello world! Hello world!"));
 app.get("/api/health", (c) => c.json({ status: "ok" }));
 
 Bun.serve({
